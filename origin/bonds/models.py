19092e3bc2e0f5fc06b003fa2b3ad6ef3b31eda4
rap-rog <juanrapmcornejo@gmail.com>
2020-10-08 11:03:39 +0100

    Added user fk to bonds

--- origin/bonds/models.py
+++ origin/bonds/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Bond(models.Model):
+    user = models.ForeignKey("auth.User", related_name="bonds", on_delete=models.CASCADE)
     isin = models.CharField(max_length=100),
     size = models.IntegerField(),
     currency = models.CharField(max_length=3),
