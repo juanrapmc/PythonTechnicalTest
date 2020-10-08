1929d97546b2bd50b2ee3c5b300413f355b0cf80
rap-rog <juanrapmcornejo@gmail.com>
2020-10-08 22:54:10 +0100

    Added url to api

--- origin/origin/urls.py
+++ origin/origin/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from bonds.views import HelloWorld
+from bonds.views import HelloWorld, BondList
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', HelloWorld.as_view())
+    path('', HelloWorld.as_view()),
+    path('bonds/', BondList.as_view()),
+    path('api-auth/', include('rest_framework.urls'))
 ]
