5f7e0d1b62def58edf79876c5e9e78a1876bcf43
rap-rog <juanrapmcornejo@gmail.com>
2020-10-08 10:58:32 +0100

    Added bond model

--- origin/bonds/models.py
+++ origin/bonds/models.py
@@ -1,1 +1,10 @@
 from django.db import models
+
+
+class Bond(models.Model):
+    isin = models.CharField(max_length=100),
+    size = models.IntegerField(),
+    currency = models.CharField(max_length=3),
+    maturity = models.DateField(),
+    lei = models.CharField(max_length=100),
+    legal_name = models.CharField(max_length=100)
