0aa97e5dc81a04e67922d1e94e293d1e5443ed0e
rap-rog <juanrapmcornejo@gmail.com>
2020-10-09 11:47:15 +0100

    Added tests

--- origin/bonds/tests.py
+++ origin/bonds/tests.py
@@ -1,6 +1,6 @@
-from rest_framework.test import APISimpleTestCase
+from rest_framework.test import APISimpleTestCase, APITestCase, APIClient
+from rest_framework.exceptions import ValidationError, APIException
 from unittest import TestCase, mock
-from unittest.mock import patch
 from bonds.utils import getLeiLegalName
 from django.test import TestCase as DTestCase
 from bonds.models import Bond
@@ -38,22 +38,57 @@ class TestUtils(TestCase):
         self.assertEqual(None, getLeiLegalName(""))
 
 
-class TestBonds(DTestCase):
+class TestBondsModel(DTestCase):
+
+    @mock.patch("bonds.models.getLeiLegalName")
+    def setUp(self, m_getLegalName):
+        self.User = get_user_model()
+        user = self.User.objects.create(username="test", password="test")
+        self.bond_data = {
+            "isin": "ISIN",
+            "size": 123,
+            "currency": "GBP",
+            "maturity": "2020-01-01",
+            "lei": "TESTLEI",
+            "user": user
+        }
+        user2 = self.User.objects.create(username="test2", password="test2")
+        m_getLegalName.return_value = "TEST VAL1"
+        Bond.objects.create(**self.bond_data)
+        self.bond_data["user"] = user2
+        m_getLegalName.return_value = "TEST VAL2"
+        Bond.objects.create(**self.bond_data)
+
+    @mock.patch("bonds.models.getLeiLegalName")
+    def test_bondCreate(self, m_getLegalName):
+        m_getLegalName.return_value = "TEST VAL"
+        self.assertEqual("TESTVAL", Bond.objects.create(**self.bond_data).legal_name)
+
+    @mock.patch("bonds.models.getLeiLegalName")
+    def test_bondCreateNoLegalName(self, m_getLegalName):
+        m_getLegalName.return_value = None
+        self.assertRaises(ValidationError, Bond.objects.create)
+
+    def test_bondUser1GetOwnData(self):
+        user = self.User.objects.get(username="test")
+        bond = Bond.objects.all().filter(user=user)
+        self.assertEqual(1, len(bond))
+        self.assertEqual("TESTVAL1", bond[0].legal_name)
+
+
+class TestBondsAPI(APITestCase):
 
     def setUp(self):
         self.User = get_user_model()
         self.User.objects.create(username="test", password="test")
 
-    @mock.patch("bonds.models.getLeiLegalName")
-    def test_bondCreate(self, m_getLegalName):
-        m_getLegalName.return_value = "TEST VAL"
+    def test_unauthenticated(self):
+        resp = self.client.get('/bonds/')
+        self.assertEqual(resp.status_code, 403)
+
+    def test_authenticated(self):
+        client = APIClient()
         user = self.User.objects.get(username="test")
-        bond = Bond.objects.create(
-            isin="ISIN",
-            size=123,
-            currency="GBP",
-            maturity="2020-01-01",
-            lei="TESTLEI",
-            user=user
-        )
-        self.assertEqual("TESTVAL", bond.legal_name)
+        client.force_authenticate(user=user)
+        resp = client.get("/bonds/")
+        self.assertEqual(resp.status_code, 200)
